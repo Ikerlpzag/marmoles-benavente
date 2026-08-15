99c9d99ed30209f0a60d8c490c6ea453075c6661
Ikerlpzag <ikerlpzag@gmail.com>
2026-08-15 16:55:12 +0200

    p

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,37 +9,31 @@ export const projects = [
   {
     id: 1,
     title: "Encimera de cocina",
-    category: "Granito",
     image: kitchen,
   },
   {
     id: 2,
     title: "Baño a medida",
-    category: "Mármol",
     image: bathroom,
   },
   {
     id: 3,
     title: "Escalera interior",
-    category: "Granito",
     image: stairs,
   },
   {
     id: 4,
     title: "Mesa de comedor",
-    category: "Persona",
     image: table,
   },
   {
     id: 5,
     title: "Lápida personalizada",
-    category: "Arte funerario",
     image: memorial,
   },
   {
     id: 6,
     title: "Chimenea",
-    category: "Mármol",
     image: fireplace,
   },
 ];
